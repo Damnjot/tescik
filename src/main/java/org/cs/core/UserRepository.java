db74a4343726b347e7ef9c9d03ded7f2764947c4
Damnjot <236475@student.pwr.edu.pl>
2018-11-30 01:31:28 +0100

    few changes in conf

--- src/main/java/org/cs/core/UserRepository.java
+++ src/main/java/org/cs/core/UserRepository.java
@@ -1,11 +1,12 @@
 package org.cs.core;
 
 import org.cs.domain.User;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Mono;
 
 @Repository
-public interface UserRepository extends ReactiveCrudRepository<User, Integer> {
+public interface UserRepository extends ReactiveMongoRepository<User, Integer> {
     Mono<Boolean> existsByUsername(String username);
 }
--- src/main/java/org/cs/main/Application.java
+++ src/main/java/org/cs/main/Application.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "org.cs")
+@EnableReactiveMongoRepositories(basePackages = "org.cs")
 public class Application {
 
     public static void main(String[] args){
